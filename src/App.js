4eced486d7c3ebfaf2b2b0c78a92b4e75ad1c212
Breno Barbieri <brenofbarbieri@gmail.com>
2022-10-07 20:44:25 -0300

    Carousel project in ReactJs

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Slide from "./Slide";
+
+function App() {
+  const slides = [
+    {
+      id: "slide1",
+      text: "Slide 1",
+    },
+    {
+      id: "slide2",
+      text: "Slide 2",
+    },
+    {
+      id: "slide3",
+      text: "Slide 3",
+    },
+  ];
+
+  return (
+    <div>
+      <Slide slides={slides} />
+    </div>
+  );
+}
+
+export default App;
